4d6e89112b6cca931bb0e955b1f8015061ee35f3
Charlie Crocker <cscrocker84@gmail.com>
2017-01-05 21:38:52 -0600

    commented out my path

--- quiz.js
+++ quiz.js
@@ -1,2 +1,7 @@
 // create variables for robot 1 & 2
 // create variables for both robot names
+//page navigation
+//all event listeners/activateEvents()
+//effects on page change, maybe time delay or some type of card manipulation
+//functions for each page
+//functions for attack, registering damage, and determining the winner
